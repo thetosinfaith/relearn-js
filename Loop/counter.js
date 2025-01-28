232fad00d0cf8b6559f0e8fbd37afa1eab6aed8b
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2025-01-29 00:22:32 +0100

    reassignment in a loop

---
+++ Loop/counter.js
@@ -0,0 +1,5 @@
+let counter = 5;
+
+for(let count = 0; count < 10; count +=2){
+    console.log(counter++)
+}
---
+++ Loop/even_numbers.js
@@ -0,0 +1,6 @@
+let even = 0;
+
+for(let number = 0; number < 100; number++){
+    console.log(even);
+    even+=2;
+}
